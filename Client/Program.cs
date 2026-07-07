a1cd38fbde06b1941db80f8f3bf813216fb595e6
EyelessCoffee <163339352+EyelessCoffee@users.noreply.github.com>
2026-07-07 17:57:56 +0100

    Publishing Project
    So far I have a server program and client program. The client can send a message to the server which is then saved to a text document

---
+++ Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+Client client = new();
+
+client.SendMessageToServer();
+
+class Client
+{
+    TcpClient client;
+
+    public Client() => ConnectToServer();
+
+    void ConnectToServer()
+    {
+        // create client from server port
+        Console.WriteLine("Connecting to server...");
+        client = new TcpClient("127.0.0.1", 5000);
+        Console.WriteLine("Connected!");
+    }
+
+    public void SendMessageToServer()
+    {
+        if(client == null) return;
+
+        // get message
+        Console.WriteLine("Send a message to the server!");
+        string message = Console.ReadLine() ?? "";
+
+        // write the encoded message to the network stream
+        NetworkStream stream = client.GetStream();
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        stream.Write(data, 0, data.Length);
+
+        Console.WriteLine("Message sent!");
+        Console.WriteLine("Would you like to send another message? y or n");
+
+        string response = Console.ReadLine() ?? string.Empty;
+
+        if(response != "y")
+        {
+            Console.WriteLine("Bye now!");
+            return;
+        }
+
+        SendMessageToServer();
+    }
+}
+
+
+
+
---
+++ Server/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Server server = new Server();
+
+class Server
+{
+    TcpListener server;
+    TcpClient client;
+
+    public Server() => OpenServer();
+
+    void OpenServer()
+    {
+        // create a new TcpListener
+        server = new TcpListener(IPAddress.Any, 5000);
+        server.Start();
+        Console.WriteLine("Server started. Waiting for a connection...");
+        // wait for client
+        client = server.AcceptTcpClient();
+        Console.WriteLine("Client connected!");
+        ListenForMessage();
+    }
+
+    void ListenForMessage()
+    {
+        // get the stream from the client and get the Byte read
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+        // decode the message
+        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        Console.WriteLine($"Received: {message}");
+        File.AppendAllText("Message.txt", $"{DateTime.Now} : {message}");
+
+        Console.WriteLine("Close Server? y or n");
+        string response = Console.ReadLine() ?? string.Empty;
+
+        if(response != "y")
+        {
+            Console.WriteLine("Bye now!");
+            return;
+        }
+
+        ListenForMessage();
+    }
+}
+
+
+
+
+
